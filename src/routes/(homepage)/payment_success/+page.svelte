5ff74f3566dd867a974e5e6771a73ed3854feec0
snehith03 <122145839+snehith03@users.noreply.github.com>
2023-11-28 14:56:00 +0530

    Update +page.svelte

--- src/routes/(homepage)/payment_success/+page.svelte
+++ src/routes/(homepage)/payment_success/+page.svelte
@@ -11,3 +11,14 @@
 		images. Thank you for choosing our service.
 	</p>
 </article>
+
+<!-- Trackdesk tracker begin -->
+<script async src="//cdn.trackdesk.com/tracking.js"></script>
+<script>
+  (function(t,d,k){(t[k]=t[k]||[]).push(d);t[d]=t[d]||t[k].f||function(){(t[d].q=t[d].q||[]).push(arguments)}})(window,"trackdesk","TrackdeskObject");
+
+  trackdesk("svcv", "conversion", {
+    "conversionType": "sale"
+  });
+</script>
+<!-- Trackdesk tracker end -->
